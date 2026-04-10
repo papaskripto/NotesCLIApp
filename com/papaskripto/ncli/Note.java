69d663e43acc2a8da84d1a327818a691bbae2558
papaskripto <bygeoffreyouma@gmail.com>
2026-04-11 02:05:15 +0300

    Create Note class

---
+++ com/papaskripto/ncli/Note.java
@@ -0,0 +1,12 @@
+import com.papaskripto.ncli.NotesApp;
+
+public class Note {
+
+	private String fileName;
+	private String content;
+
+	public Note (String fileName, String content) {
+		this.fileName = fileName;
+		this.content = content;
+	}
+}
